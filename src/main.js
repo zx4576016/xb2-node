dd6a80c847511bce8b0f8f92ae3a1837dd9fa8ce
liulin <liulin@thinkplay.co>
2022-11-18 16:20:28 +0800

    简易的本地服务

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 const http = require('http');
 const server = http.createServer((request, response) => {
-    response.write('hello');
+    const data = {
+        id: 1,
+        title: 'wudi',
+        content: 'hahahahah'
+    }
+
+    const jsonData = JSON.stringify(data);
+    response.writeHead(200, {
+        'Content-Type': 'application/json; charset=utf-8'
+    });
+    response.write(jsonData);
     response.end();
 });
 
